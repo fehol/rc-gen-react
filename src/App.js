258f58b31cdb28849f40b58e183c10b13bf49fb2
Felix Hollmann <felix.hollmann@gmx.net>
2020-09-17 10:00:51 +0200

    f Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
-import styled, { withTheme } from 'styled-components';
+import InputColor from 'react-input-color';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import ReactDom from 'react-dom';
 
-const App = () => {
-  // Create a Arrow Function and a useState the #ffffff are the Hex Colors.
+function App() {
   const [hex, setHex] = useState('#ffffff');
-  const rndHex = () => {
-    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16); // Stole this function from a forum. It opens up all possible index numbers and randomizes the #
+  const randomHex = () => {
+    const Random = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
-    setHex(randomColor); //Start the function
+    setHex(Random);
   };
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: `${hex}`,
-        minHeight: '33.34vh',
-        overflow: 'hidden',
-        zoom: 3,
-      }}
-    >
-      <h1>{hex}</h1>
-      <h4>Please choose:</h4>
-      <button class="glow-on-hover" type="button" onClick={rndHex}>
-        Randomize
-      </button>
-      <button class="glow-on-hover" type="button">
-        Luminosity
-      </button>
-      <button
-        class="glow-on-hover"
-        type="button"
-        onClick={() => navigator.clipboard.writeText(hex)}
+    <div>
+      <div
+        className="RandomColor"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: `${hex}`,
+          minHeight: '33.34vh',
+          overflow: 'hidden',
+          zoom: 3,
+        }}
       >
-        Copy # Color
-      </button>
+        <h3>Random Color Generator</h3>
+        <h4>{hex}</h4>
+        <button class="glow-on-hover" type="button" onClick={randomHex}>
+          Randomize
+        </button>
+      </div>
+      <div>
+        <select>
+          <option value="grapefruit">Grapefruit</option>
+          <option value="lime">Lime</option>
+          <option selected value="coconut">
+            Coconut
+          </option>
+          <option value="mango">Mango</option>
+        </select>
+      </div>
     </div>
   );
-};
+}
 
 export default App;
+
+//https://reactjs.org/docs/forms.html
+//https://www.youtube.com/watch?time_continue=1&v=AcOjmZrcxfM&feature=emb_logo&ab_channel=HongLy
